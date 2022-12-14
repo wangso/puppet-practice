9ee06150381fdda136df78228459344fe576c8e9
wangso <wangsongjie@gmail.com>
2022-12-14 15:17:35 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,12 +3,24 @@ node default {
 
 node 'puppetserver' {
   include role::master_server
+  file {'/root/readme':
+    ensure => file,
+    content => " This is node ${fqdn}",
+  }
 }
 
 node 'puppetclient1' {
   include role::app_server
+  file {'/root/readme':
+    ensure => file,
+    content => " This is node ${fqdn}",
+  }
 }
 
 node 'puppetclient2' {
   include role::db_server
+  file {'/root/readme':
+    ensure => file,
+    content => " This is node ${fqdn}",
+  }
 }
