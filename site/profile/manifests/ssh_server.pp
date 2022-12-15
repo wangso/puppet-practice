e28bb1ac9ed7fcb3f8ca6b623c52f5af15c4d72d
wangso <wangsongjie@gmail.com>
2022-12-14 20:12:12 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -6,9 +6,10 @@ class profile::ssh_server {
     ensure => 'running',
     enable => 'true',
   }
-  ssh_authorized_key {'ubuntu@puppetserver':
+  ssh_authorized_key {'root@puppetserver':
     ensure => 'present',
-    user   => 'ubuntu',
+    user   => 'root',
     type   => 'ssh-rsa',
     key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQCoYQ/ROk0BaUE6COTJ2iYfoLXpiioQkDefefuIqnewqqhgS0iaVSm4D1r2/N88OFR/A1FIuR6aCv56YngmMmy0AEOjIoAgEkYorFOx2BASVM4pmJ8cplhxLKLF8B50KypU1TAAtl2XPkiKXE+EIwIa5hKR8vQ2Lq/Aad3LGSl4CYabTbebB+TYX5ABd7R7c8a2xD2vzV/9YY9NhIrmaN/XTAatAN/4qYsx+YrTx7ABVYr3zRXv0LNTRlLIwHxxHiSZ3azi3MaxON1U2Z9hxxywKrwkCPsoIBJS2NJZXXh6jyAqnHaT2FwN3+HjGgJ0B3N++fPw4l00D/LEOuhtfpBIzxoDhnPZEFtyu4xaim3Ew5WROra9/FV7VOZL82c3zGbDLcuTQAirnD7rJwy4NrGlI0vrEEmBM18miYLgKYfj/+pSDMrXvgqfa2me/cGzHPV6Fkh6jouuKQqmbIeCcksYWm5CRUtyAh23+p/J6RiGeTr86/tA7R1hMt7rHA4yIr8=',
+  }
 }
